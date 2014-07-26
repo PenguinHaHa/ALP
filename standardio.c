12018260cb763f788a8b26352d09f5b322132e23
PenguinHaHa <smilezjxu@gmail.com>
2014-07-26 09:25:29 +0800

    add test case standard io

---
+++ standardio.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <unistd.h>
+
+#include "testcase.h"
+
+void stdintest(void)
+{
+  printf("This is stdin test\n");
+}
+
+void stderrtest(void)
+{
+  int count;
+
+  printf("This is stderr test\n");
+
+  count = 8;
+  while(count)
+  {
+    fprintf(stderr, ".");
+    sleep(1);
+    count--;
+  }
+  fprintf(stderr, "\n");
+
+}
+
+void stdouttest(void)
+{
+  int count;
+
+  printf("This is stdout test\n");
+
+  count = 8;
+  printf("fprintf, stdout:");
+  while(count)
+  {
+    fprintf(stdout, ".");
+    count--;
+    sleep(1);
+  }
+  printf("\n");
+
+  count = 8;
+  printf("printf:");
+  while(count)
+  {
+    printf(".");
+    count--;
+    sleep(1);
+  }
+  printf("\n");
+}
+
+void standardio(void)
+{
+  // stdin
+  stdintest();
+
+  // stdout
+  stdouttest();
+  
+  // stderr
+  stderrtest();
+}
---
+++ testcase.h
@@ -0,0 +1,9 @@
+//
+// By Penguin, 2017.7
+//
+#ifndef TEST_CASE_H
+#define TEST_CASE_H
+
+void standardio(void);
+
+#endif
